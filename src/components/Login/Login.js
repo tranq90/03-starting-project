06d672c81cc310936656b4c4b86527475cb34d7e
Quan Tran <tran.q90@gmail.com>
2021-04-15 23:27:45 -0700

    useEffect summary

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,15 +11,20 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // useEffect(() => {
+  //   console.log("effect running");
+  //   return () => {
+  //     console.log("effect cleanup")
+  //   };
+  // }, [enteredPassword]);
+
   useEffect(() => {
     const identifier = setTimeout(() => {
-      console.log('validity')
       setFormIsValid(
         enteredEmail.includes("@") && enteredPassword.trim().length > 6
       );
     }, 500);
     return () => {
-      console.log('cleanup');
       clearTimeout(identifier);
     };
   }, [enteredEmail, enteredPassword]);
